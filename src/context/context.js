f52e15fe994a1bb172123be471d0112a09ab8d35
Ameenullah <md.ameenullah.h@gmail.com>
2021-03-09 09:59:44 +0530

    context api

--- src/context/context.js
+++ src/context/context.js
@@ -1,7 +1,16 @@
-import React, { useState, useEffect } from 'react';
-import mockUser from './mockData.js/mockUser';
-import mockRepos from './mockData.js/mockRepos';
-import mockFollowers from './mockData.js/mockFollowers';
-import axios from 'axios';
+import React, { useState, useEffect } from "react";
+import mockUser from "./mockData.js/mockUser";
+import mockRepos from "./mockData.js/mockRepos";
+import mockFollowers from "./mockData.js/mockFollowers";
+import axios from "axios";
+const rootUrl = "https://api.github.com";
 
-const rootUrl = 'https://api.github.com';
+const GithubContext = React.createContext();
+
+const GithubProvider = ({ children }) => {
+  return (
+    <GithubContext.Provider value={"ameen"}>{children}</GithubContext.Provider>
+  );
+};
+
+export { GithubProvider, GithubContext };
