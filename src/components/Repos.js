5a718b125edc73616978d5cb9f9d30ddc9ea9240
Ameenullah <md.ameenullah.h@gmail.com>
2021-03-15 18:24:45 +0530

    calculating most used languages

--- src/components/Repos.js
+++ src/components/Repos.js
@@ -4,7 +4,31 @@ import { GithubContext } from "../context/context";
 import { ExampleChart, Pie3D, Column3D, Bar3D, Doughnut2D } from "./Charts";
 const Repos = () => {
   const { repos } = React.useContext(GithubContext);
+  let languages = repos.reduce((total, item) => {
+    const { language } = item;
+    if (!language) {
+      return total;
+    }
+    if (!total[language]) {
+      total[language] = { label: language, value: 1 };
+    } else {
+      total[language] = {
+        ...total[language],
+        value: total[language].value + 1,
+      };
+    }
 
+    console.log(language);
+    return total;
+  }, {});
+
+  languages = Object.values(languages)
+    .sort((a, b) => {
+      return b.value - a.value;
+    })
+    .slice(0, 5);
+
+  console.log(languages);
   const chartData = [
     { label: "HTML", value: "13" },
     { label: "CSS", value: "23" },
@@ -14,7 +38,7 @@ const Repos = () => {
   return (
     <section className="section">
       <Wrapper className="section-center">
-        <Pie3D data={chartData}></Pie3D>
+        <Pie3D data={languages}></Pie3D>
         {/* <ExampleChart data={chartData}></ExampleChart> */}
       </Wrapper>
     </section>
