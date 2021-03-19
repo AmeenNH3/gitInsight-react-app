7f697f23ee1136eaf764cd950fd646cf591fa2c7
Ameenullah <md.ameenullah.h@gmail.com>
2021-03-19 23:37:28 +0530

    auth0 login logout

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -3,9 +3,34 @@ import styled from "styled-components";
 import { useAuth0 } from "@auth0/auth0-react";
 
 const Navbar = () => {
+  const {
+    isAuthenticated,
+    loginWithRedirect,
+    logout,
+    user,
+    isLoading,
+  } = useAuth0();
+  const isUser = isAuthenticated && user;
+
   return (
     <Wrapper>
-      <h3>Ameen</h3>
+      {isUser && user.picture && <img src={user.picture} alt={user.name}></img>}
+      {isUser && user.name && (
+        <h4>
+          Welcome, <strong>{user.name.toUpperCase()}</strong>
+        </h4>
+      )}
+      {isUser ? (
+        <button
+          onClick={() => {
+            logout({ returnTo: window.location.origin });
+          }}
+        >
+          logout
+        </button>
+      ) : (
+        <button onClick={loginWithRedirect}>login</button>
+      )}
     </Wrapper>
   );
 };
--- src/index.js
+++ src/index.js
@@ -5,17 +5,21 @@ import App from "./App";
 import * as serviceWorker from "./serviceWorker";
 import { GithubProvider } from "./context/context";
 import { Auth0Provider } from "@auth0/auth0-react";
-
+//dev-zya32e51.us.auth0.com
+//IciGPaj9Dv3bFp3e6m4C3x0Xe2B3XI8l
 ReactDOM.render(
   <React.StrictMode>
-    <GithubProvider>
-      <App />
-    </GithubProvider>
+    <Auth0Provider
+      domain="dev-zya32e51.us.auth0.com"
+      clientId="IciGPaj9Dv3bFp3e6m4C3x0Xe2B3XI8l"
+      redirectUri={window.location.origin}
+    >
+      <GithubProvider>
+        <App />
+      </GithubProvider>
+    </Auth0Provider>
   </React.StrictMode>,
   document.getElementById("root")
 );
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
 serviceWorker.unregister();
